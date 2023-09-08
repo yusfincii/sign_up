e62bde64273b3f8a25db9e87884a56b26f472714
yusuf <ysfnc27@gmail.com>
2023-09-09 01:09:53 +0300

    update v2

--- app/src/main/java/com/example/signuppage/SignUpPage.kt
+++ app/src/main/java/com/example/signuppage/SignUpPage.kt
@@ -10,43 +10,84 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.DatePicker
 import android.widget.Toast
 import com.example.signuppage.databinding.FragmentSignUpPageBinding
 
 class SignUpPage : Fragment() {
+    // binding
     private lateinit var page : FragmentSignUpPageBinding
 
     private var countryList = ArrayList<String>()
     private lateinit var adapterX : ArrayAdapter<String>
+
+    // this string variable represents the invalid characters list
+    private val invalid = "0123456789/?!:;%.,"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         page = FragmentSignUpPageBinding.inflate(inflater, container, false)
 
+        page.apply {
+            buttonSubmit.setOnClickListener {
+                // name control
+                if(editTextName.text.toString().trim() == "")
+                { Toast.makeText(requireContext(), "Please enter your name!", Toast.LENGTH_SHORT).show() }
+
+                // name invalid character control
+                else if(editTextName.text.any { it in invalid })
+                { Toast.makeText(requireContext(), "Please type name without invalid characters!", Toast.LENGTH_SHORT).show() }
+
+                // surname control
+                else if(editTextSurname.text.toString().trim() == "")
+                { Toast.makeText(requireContext(), "Please enter your surname!", Toast.LENGTH_SHORT).show() }
+
+                // surname invalid character control
+                else if(editTextSurname.text.any { it in invalid })
+                { Toast.makeText(requireContext(), "Please type surname without invalid characters!", Toast.LENGTH_SHORT).show() }
+
+                // date null control
+                else if(editTextBirthDate.text.toString().trim() == "")
+                { Toast.makeText(requireContext(), "Please enter your birth date!", Toast.LENGTH_SHORT).show() }
+
+                // gender control
+                else if(!(radioButtonMale.isChecked || radioButtonFemale.isChecked))
+                { Toast.makeText(requireContext(), "Please choose gender!", Toast.LENGTH_SHORT).show() }
+
+                // confirmation control
+                else if (!checkBoxTerm.isChecked)
+                { Toast.makeText(requireContext(), "Please read and tick the confirm checkbox!", Toast.LENGTH_SHORT).show() }
+
+                // successful scenario
+                else { Toast.makeText(requireContext(), "Succesfully submitted!", Toast.LENGTH_SHORT).show() }
+            }
+        }
+
         page.switchAccountType.setOnCheckedChangeListener { buttonView, isChecked ->
-            // switch ON case
+            // switch ON case - premium
             if(isChecked){
                 page.textViewPremium.setTextColor(resources.getColor(R.color.gold))
                 page.textViewStandart.setTextColor(resources.getColor(R.color.gray))
+                Toast.makeText(requireContext(), "Account Type : PREMIUM", Toast.LENGTH_SHORT).show()
 
             }
-            // switch OFF case
+            // switch OFF case - standart
             else{
                 page.textViewPremium.setTextColor(resources.getColor(R.color.gray))
                 page.textViewStandart.setTextColor(resources.getColor(R.color.black))
+                Toast.makeText(requireContext(), "Account Type : STANDART", Toast.LENGTH_SHORT).show()
             }
         }
 
         page.editTextBirthDate.setOnClickListener{
-
+            // a calendar object
             val calendar = Calendar.getInstance()
 
             val yearX = calendar.get(Calendar.YEAR)
             val monthX = calendar.get(Calendar.MONTH)
             val dayX = calendar.get(Calendar.DAY_OF_MONTH)
 
+            // it will display current date when opened
             val datePicker = DatePickerDialog(requireContext(), DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                 val text = "$dayOfMonth-$month-$year"
                 page.editTextBirthDate.setText(text)
@@ -58,29 +99,25 @@ class SignUpPage : Fragment() {
             datePicker.show()
         }
 
+        // random countries
         countryList.add("Turkey")
         countryList.add("England")
         countryList.add("Rwanda")
         countryList.add("Iraq")
         countryList.add("Germany")
 
+        // spinner adapter
         adapterX = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,
             android.R.id.text1, countryList)
         page.spinner.adapter = adapterX
 
         page.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
-            {
-                // val selectedCOuntry = countryList[position]
-            }
-
+            { val selectedCountry = countryList[position]
+                Toast.makeText(requireContext(), "Selected country : $selectedCountry", Toast.LENGTH_SHORT).show() }
             override fun onNothingSelected(parent: AdapterView<*>?)
-            {
-                Toast.makeText(requireContext(), "Please select your country", Toast.LENGTH_SHORT).show()
-            }
-
+            { Toast.makeText(requireContext(), "Please select your country", Toast.LENGTH_SHORT).show() }
         }
-
         return page.root
     }
 }
